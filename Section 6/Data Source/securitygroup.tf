bb5942bb8366438d8142b3ffe1b000e5ef0f2f83
Ashraful Alam Shihab <ash@azsystem.com>
2025-04-20 09:30:38 +0600

    Section 6 | DataSource Lab 2

--- Section 6/Data Source/securitygroup.tf
+++ Section 6/Data Source/securitygroup.tf
@@ -6,14 +6,14 @@ data "aws_ip_ranges" "ap_southeast_ip_range" {
 resource "aws_security_group" "sg_custom_ap_south" {
     name = "sg_custom_ap_south"
 
-    ingress = {
+    ingress {
         from_port = "443"
         to_port = "443"
         protocol = "tcp"
         cidr_blocks = data.aws_ip_ranges.ap_southeast_ip_range.cidr_blocks
     }
 
-    tags = {
+    tags {
         CreateDate = data.aws_ip_ranges.ap_southeast_ip_range.create_data
         SyncToken = data.aws_ip_ranges.ap_southeast_ip_range.sync_token
     }
