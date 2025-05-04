e3879fffd2c6f6e96733cb9402cdc5e8578370f4
Ashraful Alam Shihab <ash@azsystem.com>
2025-05-04 08:00:55 +0600

    Section 7 | Launch EC2 instance using Custom VPC

---
+++ Section_7/EC2_Launch_Custom_VPC/security_group.tf
@@ -0,0 +1,25 @@
+# Security Group for Levelup VPC
+
+resource "aws_security_group" "allow-level_up_ssh" {
+  vpc_id = aws_vpc.level_up_vpc.id
+  name = "allow_level_up_ssh"
+  description = "Security group to allow SSH Connection"
+
+egress  {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+tags = {
+  Name = "allow_level_up_ssh"
+}
+
+}
